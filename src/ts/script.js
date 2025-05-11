9d7089246edf6cfecf1e10ddd21c017ea6579280
Christia Amaral <chris_lla@hotmail.com>
2025-05-11 00:55:41 -0300

    update teste

--- src/ts/script.js
+++ src/ts/script.js
@@ -10,3 +10,34 @@ botaoNao.addEventListener('mouseenter', function () {
     botaoNao.style.left = "".concat(randomX, "px");
     botaoNao.style.top = "".concat(randomY, "px");
 });
+function toggleDropdown(id) {
+    var allMenus = document.querySelectorAll('.dropdown-content');
+    allMenus.forEach(function (menu) {
+        if (menu.id === id) {
+            menu.classList.toggle('show');
+        }
+        else {
+            menu.classList.remove('show'); // Fecha os outros
+        }
+    });
+}
+window.onclick = function (event) {
+    var target = event.target;
+    if (!target.classList.contains('.btn-restaurante')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+document.addEventListener('DOMContentLoaded', function () {
+    var botao = document.getElementById('btn-s');
+    if (botao) {
+        botao.addEventListener('click', function () {
+            window.location.href = 'choice.html';
+        });
+    }
+});
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -4,6 +4,7 @@ const botaoSim = document.querySelector('.btn-s') as HTMLButtonElement;
 botaoNao.addEventListener('mouseenter', () => {
     botaoNao.style.transition = 'none';
     botaoNao.style.position = 'absolute';
+
     const maxX = window.innerWidth - botaoNao.offsetWidth;
     const maxY = window.innerHeight - botaoNao.offsetHeight;
 
@@ -12,4 +13,41 @@ botaoNao.addEventListener('mouseenter', () => {
 
     botaoNao.style.left = `${randomX}px`;
     botaoNao.style.top = `${randomY}px`;
+}); 
+
+function toggleDropdown(id: string): void {
+    const allMenus: NodeListOf<HTMLElement> = document.querySelectorAll('.dropdown-content');
+
+    allMenus.forEach((menu: HTMLElement) => {
+        if (menu.id === id) {
+            menu.classList.toggle('show');
+        } else {
+            menu.classList.remove('show'); // Fecha os outros
+        }
+    });
+}
+
+
+window.onclick = function (event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+
+    if (!target.classList.contains('.btn-restaurante')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i] as HTMLElement;
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('btn-s') as HTMLButtonElement;
+
+    if (botao) {
+        botao.addEventListener('click', () => {
+            window.location.href = 'choice.html';
+        });
+    }
 });
